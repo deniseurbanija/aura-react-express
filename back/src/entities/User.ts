de13f1c828704c911e3ce1b8587dfa80d725a029
deniseurbanija2 <deniseurbanija@outlook.com.ar>
2024-04-08 13:49:30 -0300

    operations

--- back/src/entities/User.ts
+++ back/src/entities/User.ts
@@ -1,4 +1,5 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { AppDataSource } from "../config/data.source";
 
 @Entity({
   name: "users",
--- back/src/services/user.service.ts
+++ back/src/services/user.service.ts
@@ -1,34 +1,37 @@
+import { AppDataSource } from "../config/data.source";
+import { User } from "../entities/User";
 import { IUser } from "../interfaces/IUser";
 import { userDto } from "../interfaces/dto/userDto";
 import { addCredentialService } from "./credential.service";
 
 const users: IUser[] = [];
 
-export const getUsersService = async (): Promise<IUser[]> => {
+export const getUsersService = async () => {
+  const users = await AppDataSource.getRepository(User).find();
   return users;
 };
 
 export const getUserByIdService = async (id: number) => {
-  for (const user of users) {
-    if (user.id == id) {
-      return user;
-    }
+  const user = await AppDataSource.getRepository(User).findOneBy(id);
+  if (user) {
+    return user;
+  } else {
+    return "user not found";
   }
 };
 
 export let userId: number = 1;
-export const addUserService = async (userData: userDto): Promise<IUser> => {
+export const addUserService = async (userData: userDto) => {
   const { username, password, name, email, birthdate, nDni } = userData;
   const credentialId: number = await addCredentialService(username, password);
-  const newUser: IUser = {
+  const newUser = AppDataSource.getRepository(User).create({
     id: userId,
     name,
     email,
     birthdate,
     nDni,
     credentialId,
-  };
-  users.push(newUser);
-  userId++;
-  return newUser;
+  });
+  const result = await AppDataSource.getRepository(User).save(newUser);
+  return result;
 };
