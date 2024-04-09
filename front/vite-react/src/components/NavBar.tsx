bea24d16ed569a58593c8bfd31d5302048d83b5d
deniseurbanija2 <deniseurbanija@outlook.com.ar>
2024-04-09 20:52:17 -0300

    react+vite

---
+++ front/vite-react/src/components/NavBar.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/NavBar.module.css";
+
+export const NavBar = () => {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.title}>
+        <h3>Henry Schedules</h3>
+      </div>
+      <div className={styles.ul_div}>
+        <ul className={styles.ul}>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About</li>
+          <li className={styles.yellow_btn}>My Appointments</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
---
+++ front/vite-react/src/views/Home.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../components/NavBar";
+
+function Home() {
+  return (
+    <div>
+      {<NavBar />}
+      <h1>HOME</h1>
+    </div>
+  );
+}
+export default Home;
