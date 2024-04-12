c452ac42362b9d224db31de6885699dd8d4df701
deniseurbanija2 <deniseurbanija@outlook.com.ar>
2024-04-12 15:59:41 -0300

    backend finished

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import indexRouter from "./routes/index.router";
 import morgan from "morgan";
+import cors from "cors";
 
 const server = express();
 server.use(express.json());
 server.use(morgan("dev"));
+server.use(cors());
 
 server.use((err: any, req: Request, res: Response, next: NextFunction) => {
   res.status(err.statusCode || 500).json({ error: err.message });
