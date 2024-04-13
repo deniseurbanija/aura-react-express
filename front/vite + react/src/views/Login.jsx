ec444f4f171cc47ce102d2087b85cf561e8fe537
deniseurbanija2 <deniseurbanija@outlook.com.ar>
2024-04-13 16:48:44 -0300

    forms

--- front/vite + react/src/views/Login.jsx
+++ front/vite + react/src/views/Login.jsx
@@ -1,3 +1,47 @@
-const Login = () => {}
+import { useState } from "react";
+import styles from "../styles/Register.module.css";
 
-export default Login
+const Login = () => {
+  const [loginForm, setLoginForm] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+
+    setLoginForm({
+      ...loginForm,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(loginForm)
+  };
+
+  return (
+    <form className={styles.form_container} onSubmit={handleSubmit}>
+      <label>Username</label>
+      <input
+        name="username"
+        type="text"
+        value={loginForm.username}
+        onChange={handleOnChange}
+      ></input>
+
+      <label>Password</label>
+      <input
+        name="password"
+        type="password"
+        value={loginForm.password}
+        onChange={handleOnChange}
+      ></input>
+
+      <button>Submit</button>
+    </form>
+  );
+};
+
+export default Login;
--- front/vite + react/src/views/Register.jsx
+++ front/vite + react/src/views/Register.jsx
@@ -1,34 +1,79 @@
-// import { useState } from "react";
+import { useState } from "react";
 import styles from "../styles/Register.module.css";
 
 const Register = () => {
-  //   const [registerForm, setRegisterForm] = useState({
-  //     name: "",
-  //     nDni: 0,
-  //     birthdate: "",
-  //     username: "",
-  //     password: "",
-  //   });
+  const [registerForm, setRegisterForm] = useState({
+    name: "",
+    nDni: "",
+    birthdate: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const handleOnChange = (event) => {
+    const { name, value } = event.target;
+
+    setRegisterForm({
+      ...registerForm,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(registerForm);
+  };
 
   return (
-    <form className={styles.form_container}>
+    <form className={styles.form_container} onSubmit={handleSubmit}>
       <label>Name</label>
-      <input name="name" placeholder="name"></input>
+      <input
+        name="name"
+        type="text"
+        value={registerForm.name}
+        onChange={handleOnChange}
+      ></input>
 
       <label>DNI</label>
-      <input></input>
+      <input
+        name="nDni"
+        type="text"
+        value={registerForm.nDni}
+        onChange={handleOnChange}
+      ></input>
 
       <label>Birthdate</label>
-      <input></input>
+      <input
+        name="birthdate"
+        type="text"
+        value={registerForm.birthdate}
+        onChange={handleOnChange}
+      ></input>
 
       <label>Username</label>
-      <input></input>
+      <input
+        name="username"
+        type="text"
+        value={registerForm.username}
+        onChange={handleOnChange}
+      ></input>
 
       <label>Email</label>
-      <input></input>
+      <input
+        name="email"
+        type="text"
+        value={registerForm.email}
+        onChange={handleOnChange}
+      ></input>
 
       <label>Password</label>
-      <input></input>
+      <input
+        name="password"
+        type="password"
+        value={registerForm.password}
+        onChange={handleOnChange}
+      ></input>
 
       <button>Submit</button>
     </form>
